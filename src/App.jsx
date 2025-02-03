c61bdab9b9a31fea314338a90c396df4d692f0e0
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-02-03 23:01:28 +0200

    save and fetch user places

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import ErrorPage from './components/Error.jsx';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchUserPlaces().then((placesData) => {
+      setUserPlaces(placesData.places);
+    }).catch((error) => {
+      setError({message: error.message || "Failed to fetch user places"});
+    });
+    setIsFetching(false);
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -32,6 +47,11 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    updateUserPlaces([selectedPlace, ...userPlaces]).catch((error) => {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({message: error.message || "Failed to update places."});
+    });
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
@@ -39,11 +59,27 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    updateUserPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id)).catch((error) => {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({message: error.message || "Failed to delete place"});
+    });
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
+
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
 
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && <ErrorPage
+          title="An error occurred!"
+          message={errorUpdatingPlaces.message}
+          onConfirm={handleError} />
+        }
+      </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -60,12 +96,15 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
+        {error && <ErrorPage title="An error occurred!" message={error}/>}
+        {!error && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          loadingText="Fetching your places..."
+          isLoading={isFetching}
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
-        />
+        />}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- src/http.js
+++ src/http.js
@@ -6,3 +6,27 @@ export function fetchAvailablePlaces() {
         return response.json();
     });
 };
+
+export function fetchUserPlaces() {
+    return fetch("http://localhost:3000/user-places").then((response) => {
+        if(!response.ok) {
+            throw new Error("Failed to fetch user places");
+        }
+        return response.json();
+    });
+};
+
+export function updateUserPlaces(places) {
+    return fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({places}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        if(!response.ok) {
+            throw new Error("Failed to update user data.");
+        }
+        return response.json();
+    });
+};
